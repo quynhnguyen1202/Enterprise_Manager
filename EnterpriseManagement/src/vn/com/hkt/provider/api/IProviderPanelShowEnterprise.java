f00b2adb81a76926bbdf0ee40852c2f543143dcf
Minh Thang <minhthangls089@gmail.com>
2013-03-14 14:28:02 +0700

    test

---
+++ EnterpriseManagement/src/vn/com/hkt/provider/api/IProviderPanelShowEnterprise.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vn.com.hkt.provider.api;
+
+import java.util.List;
+import vn.com.hkt.data.entity.Enterprise;
+
+/**
+ *
+ * @author Administrator
+ */
+public interface IProviderPanelShowEnterprise extends IProviderGeneral<Enterprise> {
+    
+    public List<Enterprise> getEnterpriseChildren(long idEnterprise);
+    
+}
---
+++ EnterpriseManagement/src/vn/com/hkt/provider/spi/ProviderPanelShowEnterprise.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vn.com.hkt.provider.spi;
+
+import java.util.List;
+import vn.com.hkt.dao.api.IEnterpriseDao;
+import vn.com.hkt.dao.spi.EnterpriseDao;
+import vn.com.hkt.data.entity.Enterprise;
+import vn.com.hkt.provider.api.IProviderPanelShowEnterprise;
+
+/**
+ *
+ * @author Administrator
+ */
+public class ProviderPanelShowEnterprise implements IProviderPanelShowEnterprise{
+
+    private Enterprise enterprise;
+    private IEnterpriseDao enterpriseDao;
+
+    public ProviderPanelShowEnterprise() {
+        enterpriseDao = new EnterpriseDao();
+    }
+    
+    @Override
+    public List<Enterprise> getEnterpriseChildren(long idEnterprise) {
+        return null;
+    }
+
+    @Override
+    public void setDataView(Enterprise object) {
+        this.enterprise = object;
+    }
+
+    @Override
+    public Enterprise getDataView() {
+        if(enterprise==null){
+            enterprise = new Enterprise();
+        }
+        return enterprise;
+    }
+
+    @Override
+    public long addData() {
+        if(enterprise!=null){
+            if(enterpriseDao.insert(enterprise)){
+                return enterprise.getId();
+            }else{
+                return -1;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public long deleteData() {
+        return 0;
+    }
+    
+}
