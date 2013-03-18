e841e0bb78da57f4373cee6849e84288c0c61def
quynh nguyen <quynhnt1202>
2013-03-18 08:58:08 +0700

    sua lai

--- EnterpriseManagement/src/vn/com/hkt/dao/spi/DepartmentDao.java
+++ EnterpriseManagement/src/vn/com/hkt/dao/spi/DepartmentDao.java
@@ -13,15 +13,9 @@ import vn.com.hkt.data.entity.Department;
  * @author QuynhNguyen
  */
 public class DepartmentDao extends EntityDao<Department> implements IDepartmentDao {
-    DepartmentDao departmentDao = new DepartmentDao();
+
     public DepartmentDao() {
         setClassName(Department.class);
-    
-    }
 
-    @Override
-    public List<Department> getByIdDepartment(long IdDepartment) {
-        String sql = "Select dep from Department dep where dep.IdDepartment = ?1";
-        return departmentDao.em.createQuery(sql).setParameter(1, IdDepartment).getResultList();
     }
 }
