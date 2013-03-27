0662b5b7ad608d5df757c27125c1a2d7feea9a0a
quynh nguyen <quynhnt1202>
2013-03-27 10:03:31 +0700

    sua file ProjectRevenueStatistic.java  : thong ke Project theo tuy chon idDepartment , idENterprise

--- EnterpriseManagement/src/vn/com/hkt/statistic/api/IProjectRevenueStatistic.java
+++ EnterpriseManagement/src/vn/com/hkt/statistic/api/IProjectRevenueStatistic.java
@@ -17,10 +17,22 @@ public interface IProjectRevenueStatistic {
     public float revenueGetByTotalProject(long idProject, Date dateStart, Date dateEnd);
 
     public float revenueGetByTotalChildrenProject(long idProject, Date dateStart, Date dateEnd);
-    
+
     public float spendingGetByProject(long idProject, Date dateStart, Date dateEnd);
 
     public float spendingGetByTotalProject(long idProject, Date dateStart, Date dateEnd);
 
     public float spendingGetByTotalChildrenProject(long idProject, Date dateStart, Date dateEnd);
+
+    public float revenueGetByProject(long idProject, long idDepartment, long idEnterprise, Date dateStart, Date dateEnd);
+
+    public float spendingGetByProject(long idProject, long idDepartment, long idEnterprise, Date dateStart, Date dateEnd);
+
+    public float revenueGetByTotalProject(long idProject, long idDepartment, long idEnterprise, Date dateStart, Date dateEnd);
+
+    public float spendingGetByTotalProject(long idProject, long idDepartment, long idEnterprise, Date dateStart, Date dateEnd);
+
+    public float revenueGetByTotalChildrenProject(long idProject, long idDepartment, long idEnterprise, Date dateStart, Date dateEnd);
+
+    public float spendingGetByTotalChildrenProject(long idProject, long idDepartment, long idEnterprise, Date dateStart, Date dateEnd);
 }
